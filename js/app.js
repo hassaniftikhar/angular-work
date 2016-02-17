2a239269b091681389330abca98a6c813cb890a9
Hassan Iftikhar <hassan.iftikhar.ch@gmail.com>
2016-02-17 10:59:20 +0500

    4_4 details partial added

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ myApp.config(['$routeProvider', function ($routeProvider){
 
   $routeProvider
       .when('/list', {templateUrl: 'partials/list.html', controller: 'ListController'})
+      .when('/details/:itemId', {templateUrl: 'partials/details.html', controller: 'DetailsController'})
       .otherwise({redirectTo: '/list'})
 
 }])
--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,16 @@
 var artistController = angular.module('artistController', []);
 
-  artistController.controller('ListController', ['$scope', '$http', function($scope, $http) {
-
-  $http.get('js/data.json').success(function(data){
+artistController.controller('ListController', ['$scope', '$http', function ($scope, $http) {
+  $http.get('js/data.json').success(function (data) {
     $scope.artists = data;
     $scope.artistOrder = "name"
+  });
+}]);
+
+artistController.controller('DetailsController', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+  $http.get('js/data.json').success(function (data) {
+    $scope.artists = data;
+    $scope.whichItem = $routeParams.itemId;
   });
 }]);
 
